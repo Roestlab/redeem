26cdda746aa2ee28e7165814b320f59a8ad03d33
singjc <justincsing@gmail.com>
2024-12-11 00:17:20 -0500

    add: Clone to ModelParams

--- crates/redeem-classifiers/src/psm_scorer.rs
+++ crates/redeem-classifiers/src/psm_scorer.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 use crate::data_handling::Experiment;
 use crate::models::xgboost::XGBoostClassifier;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ModelParams {
     pub learning_rate: f32,
     // XGBoost-specific parameters
