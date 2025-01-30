3267af79a55905fab090c3b84411b0afb836a019
Justin <justincsing@gmail.com>
2025-01-30 15:47:35 -0500

    update: device type retrieval

--- crates/redeem-properties/src/utils/utils.rs
+++ crates/redeem-properties/src/utils/utils.rs
@@ -1,4 +1,5 @@
 use candle_core::Device;
+use candle_core::utils::{cuda_is_available, metal_is_available};
 use anyhow::{Result, anyhow};
 
 /// Converts a device string to a Candle Device.
@@ -54,6 +55,64 @@ pub fn get_device(device_str: &str) -> Result<Device> {
         match device_str {
             "cpu" => Ok(Device::Cpu),
             _ => Err(anyhow!("Unsupported device type: {}", device_str)),
+        }
+    }
+}
+
+
+/// Returns the best available device based on the specified flags.
+/// 
+/// # Arguments
+/// 
+/// * `cpu` - A flag indicating whether to use the CPU device
+/// 
+/// # Returns
+/// 
+/// A `Result` containing the Candle `Device` or an error if the device is not available
+pub fn device(cpu: bool) -> Result<Device> {
+    if cpu {
+        Ok(Device::Cpu)
+    } else if cuda_is_available() {
+        Ok(Device::new_cuda(0)?)
+    } else if metal_is_available() {
+        Ok(Device::new_metal(0)?)
+    } else {
+        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+        {
+            println!(
+                "Running on CPU, to run on GPU(metal), build this example with `--features metal`"
+            );
+        }
+        #[cfg(not(all(target_os = "macos", target_arch = "aarch64")))]
+        {
+            println!("Running on CPU, to run on GPU, build this example with `--features cuda`");
+        }
+        Ok(Device::Cpu)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use candle_core::utils::{cuda_is_available, metal_is_available};
+    use candle_core::{Device, Result, Tensor};
+
+    #[test]
+    fn test_device(){
+        let device = get_device("cpu").unwrap();
+        println!("Device: {:?}", device);
+
+        if cuda_is_available() {
+            let device = get_device("cuda").unwrap();
+            println!("Device: {:?}", device);
+        } else {
+            println!("CUDA is not available");
+        }
+
+        if metal_is_available() {
+            let device = get_device("metal").unwrap();
+            println!("Device: {:?}", device);
         }
     }
 }
