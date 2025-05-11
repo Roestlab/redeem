5891b4e3c0f4954c1c76bf4d1b3d97941b295cf9
singjc <justincsing@gmail.com>
2025-05-11 02:40:52 -0400

    refactor: Add precursor mass field to PeptideData struct in redeem-properties crate

--- crates/redeem-properties/src/utils/data_handling.rs
+++ crates/redeem-properties/src/utils/data_handling.rs
@@ -3,22 +3,26 @@
 pub struct PeptideData {
     pub sequence: String,
     pub charge: Option<i32>,
+    pub precursor_mass: Option<f32>,
     pub nce: Option<i32>,
     pub instrument: Option<String>,
     pub retention_time: Option<f32>,
     pub ion_mobility: Option<f32>,
+    pub ccs: Option<f32>,
     pub ms2_intensities: Option<Vec<Vec<f32>>>,
 }
 
 impl PeptideData {
-    pub fn new(sequence: &str, charge: Option<i32>, nce: Option<i32>, instrument: Option<&str>, retention_time: Option<f32>, ion_mobility: Option<f32>, ms2_intensities: Option<Vec<Vec<f32>>>) -> Self {
+    pub fn new(sequence: &str, charge: Option<i32>, precursor_mass: Option<f32>, nce: Option<i32>, instrument: Option<&str>, retention_time: Option<f32>, ion_mobility: Option<f32>, ccs: Option<f32>, ms2_intensities: Option<Vec<Vec<f32>>>) -> Self {
         Self {
             sequence: sequence.to_string(),
             charge,
+            precursor_mass,
             nce,
             instrument: instrument.map(|s| s.to_string()),
             retention_time,
             ion_mobility,
+            ccs,
             ms2_intensities
         }
     }
